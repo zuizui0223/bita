5fef6c2300df0f803c6c82dd3f73b0009d28baa2
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-07-13 19:04:06 +0900

    Export the explicit oriented-curvature API

--- trait_architecture/__init__.py
+++ trait_architecture/__init__.py
@@ -1,7 +1,7 @@
 """Active theory API for the submission supplement."""
 
 from .model import Architecture, FitnessBreakdown, InteractionRegime, ModelParameters, fitness
-from .sign_criterion import LocalRegimeCriterion, RegimeScaledCriterion, SignCriterion
+from .sign_criterion import LocalRegimeCriterion, OrientedSignCriterion, RegimeScaledCriterion
 
 __all__ = [
     "Architecture",
@@ -9,7 +9,7 @@ __all__ = [
     "InteractionRegime",
     "ModelParameters",
     "fitness",
-    "SignCriterion",
+    "OrientedSignCriterion",
     "RegimeScaledCriterion",
     "LocalRegimeCriterion",
 ]
